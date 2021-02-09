fba27fffb27a5ec9f891b1410dd55ee002cf51bc
ben-www <weinstock.ba@gmail.com>
2021-02-09 17:34:52 -0500

    Initial Commit

---
+++ BasketballCards/BasketballCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballCardsApp.swift
+//  BasketballCards
+//
+//  Created by Benjamin Weinstock on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct BasketballCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
